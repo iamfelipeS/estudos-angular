70b4c2317fe4d44332487f376ea4200efce17083
Felipe <lipe1090@icloud.com>
2025-05-27 10:57:04 -0300

    add: desafio 2 completo

--- src/app/views/manipulacao-dados/desafio-02/desafio-02.component.ts
+++ src/app/views/manipulacao-dados/desafio-02/desafio-02.component.ts
@@ -1,11 +1,25 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-desafio-02',
-  imports: [],
+  imports: [CommonModule, FormsModule,],
   templateUrl: './desafio-02.component.html',
   styleUrl: './desafio-02.component.scss'
 })
 export class Desafio02Component {
+  valorProduto: number = 0;
+  valorComDesconto: number = 0;
+  descontoPercentual: number = 10;
+
+produtoComDesconto() {
+  if (this.descontoPercentual > 100) {
+    this.descontoPercentual = 100;
+  }
+  const fatorDesconto = 1 - this.descontoPercentual / 100;
+  this.valorComDesconto = this.valorProduto * fatorDesconto;
+}
+
 
 }
